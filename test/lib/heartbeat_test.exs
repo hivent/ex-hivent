b337825e4e34bb25950733b126647cadd233611e
Bruno Abrantes <bruno@brunoabrantes.com>
2016-11-19 15:04:21 +0100

    Implements a Heartbeat Process

---
+++ test/lib/heartbeat_test.exs
@@ -0,0 +1,42 @@
+defmodule HiventHeartbeatTest do
+  use ExUnit.Case
+  doctest Hivent.Heartbeat
+
+  @consumer "a_consumer"
+  @dead_consumer "dead_consumer"
+  @interval 10
+
+  setup do
+    {:ok, pid} = Hivent.Heartbeat.start_link(@consumer, @interval)
+    redis = Process.whereis(:redis)
+
+    redis |> Exredis.Api.flushall
+
+    service = Hivent.Config.get(:hivent, :client_id)
+
+    [pid: pid, redis: redis, service: service]
+  end
+
+  test "keeps the consumer alive", %{redis: redis, service: service} do
+    key = "#{service}:#{@consumer}:alive"
+    alive = redis |> Exredis.Api.get(key)
+
+    assert alive == "true"
+
+    round(@interval * 1.5) |> Process.sleep()
+
+    alive = redis |> Exredis.Api.get(key)
+
+    assert alive == "true"
+  end
+
+  test "cleans up dead consumers", %{redis: redis, service: service} do
+    redis |> Exredis.Api.sadd("#{service}:consumers", @dead_consumer)
+
+    round(@interval * 1.5) |> Process.sleep()
+
+    consumers = redis |> Exredis.Api.smembers("#{service}:consumers")
+
+    assert consumers == [@consumer]
+  end
+end
