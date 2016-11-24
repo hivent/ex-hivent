a0d4e4563d373721694e6e185849c1302a7cc19d
Bruno Abrantes <bruno@brunoabrantes.com>
2016-11-24 17:36:42 +0100

    Ignores file for linting

--- lib/hivent/config.ex
+++ lib/hivent/config.ex
@@ -2,6 +2,7 @@ defmodule Hivent.Config do
   @moduledoc """
   This module handles fetching values from the config with some additional niceties
   """
+  @lint false
 
   @doc """
   Fetches a value from the config, or from the environment if {:system, "VAR"}
