73f6cc962801365bba125f1b161caada8302bca1
Bruno Abrantes <bruno@brunoabrantes.com>
2017-02-23 22:34:42 +0100

    Releases v1.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hivent.Mixfile do
 
   def project do
     [app: :hivent,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
